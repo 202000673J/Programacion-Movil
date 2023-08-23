339718c32aaef5e997d99fe994a95de509ecb8e1
janCarlos <202000673J@utea.edu.pe>
2023-08-23 06:49:50 -0500

    ejercisios_dart

--- labot.dart
+++ labot.dart
@@ -1,21 +1,4 @@
-// A function declaration.
-int y(int x) {
-  return x * 2;
-}
-
-// Arrow syntax is shorthand for `{ return expr; }`.
-int z(int x) => y(y(x));
-
-// Functions are objects.
-int w(int x, int Function(int) f) {
-  for (var i = 0; i < 2; i++) {
-    x = f(x);
-  }
-  return x;
-}
-
-void main() {
-  print('4 times two is ${y(4)}');
-  print('4 times four is ${z(4)}');
-  print('2 x 2 x 2 is ${w(2, y)}');
+import 'dart:io';
+void main(List<String> args) {
+  stdout.write("")
 }
