ae0a2c7ae5a85957be7a48d4f20a4ddca9aae2a0
janCarlos <202000673J@utea.edu.pe>
2023-08-23 08:12:40 -0500

    clase_8

--- clase_8.dart
+++ clase_8.dart
@@ -1,10 +1,20 @@
 void main(List<String> args) {
-  multi(5);
+  //multi(9);
+  String? nombre;
+  print(nombre);
+
+  int? y;
+  int w = y! + 5;
+  print(w);
+
+  String? apell;
+  String apellformat = nombre ?? "buleje";
+  print(apellformat);
 }
 
 void multi(int x) {
-  List e = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  List e = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   for (var i = 0; i < e.length; i++) {
-    print(x * i);
+    print("$x * ${e[i]} = ${x * i}");
   }
 }
